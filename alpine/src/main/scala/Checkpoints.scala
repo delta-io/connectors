48dc2974ab5a7f1a5a9de4badc49df8a3b3c4f68
Scott Sandre <scott.sandre@databricks.com>
2020-09-11 14:03:40 -0400

    added copyright meta text

--- alpine/src/main/scala/Checkpoints.scala
+++ alpine/src/main/scala/Checkpoints.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright (2020) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main.scala
 
 import java.io.FileNotFoundException
@@ -70,7 +86,7 @@ trait Checkpoints {
   /** The path to the file that holds metadata about the most recent checkpoint. */
   val LAST_CHECKPOINT = new Path(logPath, "_last_checkpoint")
 
-  private[delta] def lastCheckpoint: Option[CheckpointMetaData] = {
+  private def lastCheckpoint: Option[CheckpointMetaData] = {
     loadMetadataFromFile(0)
   }
 
