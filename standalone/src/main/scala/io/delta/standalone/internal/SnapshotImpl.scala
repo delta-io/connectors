d0104eb2c79d2147035f2ef0899dc3ec634d1b62
Yuhong Chen <mikechen212@gmail.com>
2021-06-14 18:40:49 -0700

    Update SnapshotImpl.scala

--- standalone/src/main/scala/io/delta/standalone/internal/SnapshotImpl.scala
+++ standalone/src/main/scala/io/delta/standalone/internal/SnapshotImpl.scala
@@ -95,7 +95,7 @@ private[internal] class SnapshotImpl(
           JsonUtils.mapper.readValue[SingleAction](line)
         }
       } else if (path.endsWith("parquet")) {
-        ParquetReader.read[SingleAction](path, ParquetReader.Options(hadoopConf = hadoopConf)).toSeq
+        ParquetReader.read[SingleAction](path, ParquetReader.Options(timeZone = readTimeZone, hadoopConf = hadoopConf)).toSeq
       } else Seq.empty[SingleAction]
     }.toList
   }
