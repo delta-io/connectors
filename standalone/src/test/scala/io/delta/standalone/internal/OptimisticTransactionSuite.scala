44564e137082a3b61b31e23072e7e80ffa6c8d0b
Scott Sandre <scott.sandre@databricks.com>
2021-10-07 10:26:53 -0700

    Update OptimisticTransactionSuite.scala

--- standalone/src/test/scala/io/delta/standalone/internal/OptimisticTransactionSuite.scala
+++ standalone/src/test/scala/io/delta/standalone/internal/OptimisticTransactionSuite.scala
@@ -20,13 +20,17 @@ import java.util.Collections
 
 import scala.collection.JavaConverters._
 
-import io.delta.standalone.actions.{AddFile => AddFileJ, Metadata => MetadataJ, SetTransaction => SetTransactionJ, RemoveFile => RemoveFileJ}
+import io.delta.standalone.actions.{Protocol, AddFile => AddFileJ, Metadata => MetadataJ, RemoveFile => RemoveFileJ, SetTransaction => SetTransactionJ}
 import io.delta.standalone.expressions.{EqualTo, Literal}
 import io.delta.standalone.types.{IntegerType, StructField, StructType}
 
 class OptimisticTransactionSuite extends OptimisticTransactionSuiteBase {
   private val addA = new AddFileJ("a", Collections.emptyMap(), 1, 1, true, null, null)
   private val addB = new AddFileJ("b", Collections.emptyMap(), 1, 1, true, null, null)
+
+  private val addA_partX1 = new AddFileJ("a", Map("x" -> "1").asJava, 1, 1, true, null, null)
+  private val addA_partX2 = new AddFileJ("a", Map("x" -> "2").asJava, 1, 1, true, null, null)
+  private val schema = new StructType(Array(new StructField("x", new IntegerType())))
 
   /* ************************** *
    * Allowed concurrent actions *
@@ -35,13 +39,9 @@ class OptimisticTransactionSuite extends OptimisticTransactionSuiteBase {
   check(
     "append / append",
     conflicts = false,
-    reads = Seq(
-      t => t.metadata()
-    ),
-    concurrentWrites = Seq(
-      addA),
-    actions = Seq(
-      addB))
+    reads = Seq(t => t.metadata()),
+    concurrentWrites = Seq(addA),
+    actions = Seq(addB))
 
   check(
     "disjoint txns",
@@ -53,49 +53,133 @@ class OptimisticTransactionSuite extends OptimisticTransactionSuiteBase {
       new SetTransactionJ("t2", 0, java.util.Optional.of(1234L))),
     actions = Nil)
 
-  {
-    val schema = new StructType(Array(new StructField("x", new IntegerType())))
+  check(
+    "disjoint delete / read",
+    conflicts = false,
+    setup = Seq(
+      MetadataJ.builder().schema(schema).partitionColumns(Seq("x").asJava).build(),
+      addA_partX2
+    ),
+    reads = Seq(
+      t => t.markFilesAsRead(new EqualTo(schema.column("x"), Literal.of(1)))
+    ),
+    concurrentWrites = Seq(
+      RemoveFileJ.builder("a").deletionTimestamp(4L).build()
+    ),
+    actions = Seq()
+  )
 
-    check(
-      "disjoint delete / read",
-      conflicts = false,
-      setup = Seq(
-        MetadataJ.builder()
-          .schema(schema)
-          .partitionColumns(Seq("x").asJava)
-          .build(),
-        new AddFileJ("a", Map("x" -> "2").asJava, 1, 1, true, null, null)
-      ),
-      reads = Seq(
-        t => t.markFilesAsRead(new EqualTo(schema.column("x"), Literal.of(1)))
-      ),
-      concurrentWrites = Seq(
-        RemoveFileJ.builder("a").deletionTimestamp(4L).build()
-      ),
-      actions = Seq()
-    )
-  }
+  check(
+    "disjoint add / read",
+    conflicts = false,
+    setup = Seq(
+      MetadataJ.builder().schema(schema).partitionColumns(Seq("x").asJava).build()
+    ),
+    reads = Seq(
+      t => t.markFilesAsRead(new EqualTo(schema.column("x"), Literal.of(1)))
+    ),
+    concurrentWrites = Seq(
+      addA_partX2
+    ),
+    actions = Seq()
+  )
 
-  {
-    val schema = new StructType(Array(new StructField("x", new IntegerType())))
+  check(
+    "add / read + no write",  // no write = no real conflicting change even though data was added
+    conflicts = false,        // so this should not conflict
+    setup = Seq(
+      MetadataJ.builder().schema(schema).partitionColumns(Seq("x").asJava).build()
+    ),
+    reads = Seq(
+      t => t.markFilesAsRead(new EqualTo(schema.column("x"), Literal.of(1)))
+    ),
+    concurrentWrites = Seq(addA_partX1),
+    actions = Seq())
 
-    check(
-      "disjoint add / read",
-      conflicts = false,
-      setup = Seq(
-        MetadataJ.builder()
-          .schema(schema)
-          .partitionColumns(Seq("x").asJava)
-          .build()
-      ),
-      reads = Seq(
-        t => t.markFilesAsRead(new EqualTo(schema.column("x"), Literal.of(1)))
-      ),
-      concurrentWrites = Seq(
-        new AddFileJ("a", Map("x" -> "2").asJava, 1, 1, true, null, null)
-      ),
-      actions = Seq()
-    )
-  }
+  /* ***************************** *
+   * Disallowed concurrent actions *
+   * ***************************** */
 
+  check(
+    "delete / delete",
+    conflicts = true,
+    reads = Nil,
+    concurrentWrites = Seq(
+      RemoveFileJ.builder("a").deletionTimestamp(4L).build()),
+    actions = Seq(
+      RemoveFileJ.builder("a").deletionTimestamp(4L).build()))
+
+  check(
+    "add / read + write",
+    conflicts = true,
+    setup = Seq(
+      MetadataJ.builder().schema(schema).partitionColumns(Seq("x").asJava).build()
+    ),
+    reads = Seq(
+      t => t.markFilesAsRead(new EqualTo(schema.column("x"), Literal.of(1)))
+    ),
+    concurrentWrites = Seq(addA_partX1),
+    actions = Seq(addA_partX1),
+    // commit info should show operation as "Manual Update", because that's the operation used by
+    // the harness
+    errorMessageHint = Some("[x=1]" :: "Manual Update" :: Nil))
+
+  check(
+    "delete / read",
+    conflicts = true,
+    setup = Seq(
+      MetadataJ.builder().schema(schema).partitionColumns(Seq("x").asJava).build(),
+      addA_partX1
+    ),
+    reads = Seq(
+      t => t.markFilesAsRead(new EqualTo(schema.column("x"), Literal.of(1)))
+    ),
+    concurrentWrites = Seq(
+      RemoveFileJ.builder("a").deletionTimestamp(4L).build()
+    ),
+    actions = Seq(),
+    errorMessageHint = Some("a in partition [x=1]" :: "Manual Update" :: Nil))
+
+  check(
+    "schema change",
+    conflicts = true,
+    reads = Seq(
+      t => t.metadata
+    ),
+    concurrentWrites = Seq(MetadataJ.builder().build()),
+    actions = Nil)
+
+  check(
+    "conflicting txns",
+    conflicts = true,
+    reads = Seq(
+      t => t.txnVersion("t1")
+    ),
+    concurrentWrites = Seq(
+      new SetTransactionJ("t1", 0, java.util.Optional.of(1234L))
+    ),
+    actions = Nil)
+
+  check(
+    "upgrade / upgrade",
+    conflicts = true,
+    reads = Seq(
+      t => t.metadata
+    ),
+    concurrentWrites = Seq(
+      new Protocol()),
+    actions = Seq(
+      new Protocol()))
+
+  // TODO: taint whole table
+
+  // TODO: taint whole table + concurrent remove
+
+  // TODO: initial commit without metadata should fail
+
+  // TODO: initial commit with multiple metadata actions should fail
+
+  // TODO: AddFile with different partition schema compared to metadata should fail
+
+  // TODO: isolation level shouldn't be null ?
 }
