819972c0effb3f6830958ed80b784189411474c2
Paweł Kubit <pawel.kubit@getindata.com>
2021-10-26 00:20:33 +0200

    Complete support for partitioned tables and add basic unit test for global committer

--- flink-connector/src/test/java/org/apache/flink/connector/delta/sink/committer/DeltaGlobalCommitterTest.java
+++ flink-connector/src/test/java/org/apache/flink/connector/delta/sink/committer/DeltaGlobalCommitterTest.java
@@ -18,28 +18,132 @@
 
 package org.apache.flink.connector.delta.sink.committer;
 
+import io.delta.standalone.DeltaLog;
+import io.delta.standalone.Snapshot;
+import io.delta.standalone.actions.AddFile;
+import io.delta.standalone.data.CloseableIterator;
+import org.apache.flink.connector.delta.sink.SchemaConverter;
+import org.apache.flink.connector.delta.sink.committables.DeltaCommittable;
+import org.apache.flink.connector.delta.sink.committables.DeltaGlobalCommittable;
+import org.apache.flink.connector.delta.sink.utils.DeltaSinkTestUtils.HadoopConfTest;
+import org.apache.flink.connector.delta.sink.utils.DeltaSinkTestUtils.TestDeltaCommittable;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.table.types.logical.IntType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.VarCharType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for {@link DeltaCommitter}.
  */
+// TODO refactor this test for more code reuse and DRY
+// TODO cover all corner cases
 public class DeltaGlobalCommitterTest {
 
-    @Test
-    public void testCommitToDeltaLogAppendMode() throws Exception {
+    @ClassRule
+    public static final TemporaryFolder TEMPORARY_FOLDER = new TemporaryFolder();
+
+    // TODO move setup for initial test Delta table to the TestUtils class
+    private static final RowType ROW_TYPE = new RowType(Arrays.asList(
+            new RowType.RowField("name", new VarCharType(VarCharType.MAX_LENGTH)),
+            new RowType.RowField("surname", new VarCharType(VarCharType.MAX_LENGTH)),
+            new RowType.RowField("age", new IntType())
+    ));
+
+    private Path tablePath;
+    private DeltaLog deltaLog;
+
+    @Before
+    public void setup() throws IOException {
+        tablePath = new Path(TEMPORARY_FOLDER.newFolder().toURI());
+        deltaLog = DeltaLog.forTable(HadoopConfTest.getHadoopConf(), tablePath.getPath());
+    }
+
+    @After
+    public void teardown() {
 
     }
 
     @Test
-    public void testCommitToDeltaLogCompleteMode() throws Exception {
+    public void testCommitToNewDeltaTableInAppendMode() throws Exception {
+        //GIVEN
+        LinkedHashMap<String, String> emptyPartitionSpec = new LinkedHashMap<>();
+        DeltaGlobalCommitter globalCommitter = new DeltaGlobalCommitter(HadoopConfTest.getHadoopConf(), tablePath, ROW_TYPE, false);
+        List<DeltaCommittable> deltaCommittables = Arrays.asList(
+                TestDeltaCommittable.getTestDeltaCommittableWithPendingFile(emptyPartitionSpec),
+                TestDeltaCommittable.getTestDeltaCommittableWithPendingFile(emptyPartitionSpec),
+                TestDeltaCommittable.getTestDeltaCommittableWithPendingFile(emptyPartitionSpec)
+        );
+        List<DeltaGlobalCommittable> globalCommittables = Arrays.asList(
+                new DeltaGlobalCommittable(deltaCommittables)
+        );
+
+        // WHEN
+        globalCommitter.commit(globalCommittables);
+
+        // THEN
+        assertEquals(deltaLog.snapshot().getVersion(), -1);
+        Snapshot snapshot = deltaLog.update();
+        assertEquals(snapshot.getVersion(), 0);
+        assertEquals(snapshot.getAllFiles().size(), deltaCommittables.size());
+        assertEquals(snapshot.getMetadata().getSchema().toJson(), new SchemaConverter().toDeltaFormat(ROW_TYPE).toJson());
+        assertTrue(snapshot.getMetadata().getPartitionColumns().isEmpty());
+        CloseableIterator<AddFile> filesIterator = snapshot.scan().getFiles();
+        while (filesIterator.hasNext()) {
+            AddFile addFile = filesIterator.next();
+            assertTrue(addFile.getPartitionValues().isEmpty());
+            assertTrue(addFile.getSize() > 0);
+            assert (!addFile.getPath().isEmpty());
+        }
 
     }
 
     @Test
-    public void testPrepareDeltaLogOperation() throws Exception {
+    public void testCommitToNewDeltaTableInAppendModeWithPartitionColumns() throws Exception {
+        //GIVEN
+        LinkedHashMap<String, String> partitionSpec = new LinkedHashMap<String, String>() {{
+            put("a", "b");
+            put("c", "d");
+        }};
+        DeltaGlobalCommitter globalCommitter = new DeltaGlobalCommitter(HadoopConfTest.getHadoopConf(), tablePath, ROW_TYPE, false);
+        List<DeltaCommittable> deltaCommittables = Arrays.asList(
+                TestDeltaCommittable.getTestDeltaCommittableWithPendingFile(partitionSpec),
+                TestDeltaCommittable.getTestDeltaCommittableWithPendingFile(partitionSpec),
+                TestDeltaCommittable.getTestDeltaCommittableWithPendingFile(partitionSpec)
+        );
+        List<DeltaGlobalCommittable> globalCommittables = Arrays.asList(
+                new DeltaGlobalCommittable(deltaCommittables)
+        );
+
+        // WHEN
+        globalCommitter.commit(globalCommittables);
+
+        // THEN
+        assertEquals(deltaLog.snapshot().getVersion(), -1);
+        Snapshot snapshot = deltaLog.update();
+        assertEquals(snapshot.getVersion(), 0);
+        assertEquals(snapshot.getAllFiles().size(), deltaCommittables.size());
+        assertEquals(snapshot.getMetadata().getPartitionColumns(), Arrays.asList("a", "c"));
+        CloseableIterator<AddFile> filesIterator = snapshot.scan().getFiles();
+        while (filesIterator.hasNext()) {
+            AddFile addFile = filesIterator.next();
+            assertEquals(addFile.getPartitionValues(), partitionSpec);
+            assertTrue(addFile.getSize() > 0);
+            assert (!addFile.getPath().isEmpty());
+        }
 
     }
-
-
 
 }
