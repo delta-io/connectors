7f865d9d5c0aeaa05d1ec9910dc14f771909ee52
allisonport-db <allison.portis@databricks.com>
2021-12-13 19:10:01 -0800

    filter out synthetic constructors

--- standalone/src/test/scala/io/delta/standalone/internal/ActionBuildersSuite.scala
+++ standalone/src/test/scala/io/delta/standalone/internal/ActionBuildersSuite.scala
@@ -64,9 +64,13 @@ class ActionBuildersSuite extends FunSuite {
 
   test("Metadata constructor matches Metadata.Builder constructor") {
     assert(
-      classOf[MetadataJ].getDeclaredConstructors.map(_.getParameterCount()).toList.max ==
-      classOf[MetadataJ.Builder].getDeclaredConstructors.map(_.getParameterCount()).toList.max,
-      "Metadata and Metadata.Builder's constructors are not the same. Please update them" +
+      classOf[MetadataJ].getDeclaredConstructors
+        .filter(!_.isSynthetic)
+        .map(_.getParameterCount()).toList.max ==
+      classOf[MetadataJ.Builder].getDeclaredConstructors
+        .filter(!_.isSynthetic)
+        .map(_.getParameterCount()).toList.max,
+      "Metadata and Metadata.Builder's constructors are not the same. Please update them " +
         "accordingly if you add a new field to Metadata."
     )
   }
