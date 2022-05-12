7748b615fbe90848d1897e69b99ca22a692843ca
Pablo Flores <pablo.flores@cerby.com>
2022-05-12 14:02:44 -0700

    Pass full table path using Path.toString
    Signed-off-by: Pablo Flores <pablofloreshernandez@hotmail.com>

--- flink/src/main/java/io/delta/flink/sink/internal/committer/DeltaGlobalCommitter.java
+++ flink/src/main/java/io/delta/flink/sink/internal/committer/DeltaGlobalCommitter.java
@@ -212,7 +212,7 @@ public class DeltaGlobalCommitter
         if (appId != null) { // means there are committables to process
             SortedMap<Long, List<DeltaCommittable>> committablesPerCheckpoint =
                 groupCommittablesByCheckpointInterval(globalCommittables);
-            DeltaLog deltaLog = DeltaLog.forTable(conf, basePath.getPath());
+            DeltaLog deltaLog = DeltaLog.forTable(conf, basePath.toString());
 
             for (long checkpointId : committablesPerCheckpoint.keySet()) {
                 OptimisticTransaction transaction = deltaLog.startTransaction();
