f94762240a90e15b4b73972f1947b091ff056c22
Jiawei Bao <jiawei.bao@databricks.com>
2022-07-15 16:18:35 -0700

    add tests cross partition & non-partition column queries, verify the columns in column-stats-filter before expr eval, precheck before get value from row record

--- standalone/src/test/scala/io/delta/standalone/internal/DataSkippingSuite.scala
+++ standalone/src/test/scala/io/delta/standalone/internal/DataSkippingSuite.scala
@@ -20,47 +20,49 @@ import org.apache.hadoop.conf.Configuration
 import org.scalatest.FunSuite
 
 import io.delta.standalone.{DeltaLog, Operation}
-import io.delta.standalone.expressions.{And, EqualTo, GreaterThanOrEqual, Literal}
-import io.delta.standalone.types.{LongType, StringType, StructField, StructType}
+import io.delta.standalone.expressions.{And, EqualTo, Expression, Literal}
+import io.delta.standalone.types.{LongType, StructField, StructType}
 
 import io.delta.standalone.internal.actions.{Action, AddFile, Metadata}
-import io.delta.standalone.internal.util.DataSkippingUtils
 import io.delta.standalone.internal.util.TestUtils._
 
 class DataSkippingSuite extends FunSuite {
   private val op = new Operation(Operation.Name.WRITE)
 
   private val partitionSchema = new StructType(Array(
-    new StructField("col2", new LongType(), true)
+    new StructField("col1", new LongType(), true)
   ))
 
   private val schema = new StructType(Array(
     new StructField("col1", new LongType(), true),
-    new StructField("col2", new LongType(), true)
+    new StructField("col2", new LongType(), true),
+    new StructField("col3", new LongType(), true)
   ))
 
-  val metadata: Metadata = Metadata(
-    partitionColumns = partitionSchema.getFieldNames, schemaString = schema.toJson)
-
-
+  val metadata: Metadata = Metadata(partitionColumns = partitionSchema.getFieldNames,
+    schemaString = schema.toJson)
 
   private val files = (1 to 20).map { i =>
-    val col1Value = (i % 3).toString
-    val col2Value = (i % 2).toString
-    val partitionValues = Map("col2" -> col2Value)
+    val col1Value = (i % 2).toString
+    val col2Value = (i % 3).toString
+    val col3Value = (i % 4).toString
+    val partitionValues = Map("col1" -> col1Value)
     val flatColumnStats = ("\"" + s"""
       | {
       |   "minValues": {
       |     "col1":$col1Value,
-      |     "col2":$col2Value
+      |     "col2":$col2Value,
+      |     "col3":$col3Value
       |   },
       |   "maxValues": {
       |     "col1":$col1Value,
-      |     "col2":$col2Value
+      |     "col2":$col2Value,
+      |     "col3":$col3Value
       |   },
       |   "nullCount": {
       |     "col1": 0,
-      |     "col2": 0
+      |     "col2": 0,
+      |     "col3": 0
       |   },
       |   "numRecords":1
       | }
@@ -70,9 +72,9 @@ class DataSkippingSuite extends FunSuite {
     AddFile(i.toString, partitionValues, 1L, 1L, dataChange = true, stats = flatColumnStats)
   }
 
-  private val metadataConjunct = new EqualTo(schema.column("col2"), Literal.of(1L))
+  private val metadataConjunct = new EqualTo(schema.column("col1"), Literal.of(1L))
 
-  private val dataConjunct = new EqualTo(schema.column("col1"), Literal.of(1L))
+  private val dataConjunct = new EqualTo(schema.column("col2"), Literal.of(1L))
 
   def withLog(actions: Seq[Action])(test: DeltaLog => Unit): Unit = {
     withTempDir { dir =>
@@ -84,30 +86,59 @@ class DataSkippingSuite extends FunSuite {
     }
   }
 
+  def withDeltaLog (l: DeltaLog => Unit): Unit = {
+    withTempDir { dir =>
+      val log = DeltaLog.forTable(new Configuration(), dir.getCanonicalPath)
+      log.startTransaction().commit(metadata :: Nil, op, "engineInfo")
+      log.startTransaction().commit(files, op, "engineInfo")
+      l(log)
+    }
+  }
+
+  def testExpression(expr: Expression, target: Seq[String]): Unit = {
+    withDeltaLog { log =>
+      val scan = log.update().scan(expr)
+      val iter = scan.getFiles
+      var resFiles: Seq[String] = Seq()
+      while (iter.hasNext) {
+        // get the index of accepted files
+        resFiles = resFiles :+ iter.next().getPath
+      }
+      assert(resFiles == target)
+    }
+  }
+
+  /** Integration test */
   // A sketchy demo:
   //
-  // table schema: (col1: int, col2: int), col2 is partition column
+  // table schema: (col1: int, col2: int) s partition column
   //
   // `files`: rows of data in table, for the i-th file in `files`,
-  //    file.path = i, file.col1 = i % 3, file.col2 = i % 2
+  //    file.path = i, file.col1 = i % 2, file.col2 = i % 3
   //
   // range of `i` is from 1 to 20.
   //
   // the query predicate is `col1 = 1 AND col2 = 1`
-  // `metadataConjunct`: the partition predicate expr, which is `col2 = 1`
-  // `dataConjunct`: the non-partition predicate expr, which is `col1 = 1`
+  // `metadataConjunct`: the partition predicate expr, which is `col1 = 1`
+  // `dataConjunct`: the non-partition predicate expr, which is `col2 = 1`
   //
-  // the accepted files should meet the condition: (i % 3 = 1 AND i % 2 = 1)
+  // the accepted files' number should meet the condition: (i % 3 == 1 AND i % 2 == 1)
   // The output should be: 1, 7, 13, 19.
-  withLog(files) { log =>
-    val scan = log.update().scan(new And(dataConjunct, metadataConjunct))
-    val iter = scan.getFiles
-    var resFiles: Seq[String] = Seq()
-    while (iter.hasNext) {
-      // get the index of accepted files
-      resFiles = resFiles :+ iter.next().getPath
-    }
-    assert(resFiles.length == 4)
-    assert(resFiles == Seq("1", "7", "13", "19"))
+  test("test column stats filter on 1 partition and 1 non-partition column") {
+    testExpression(new And(metadataConjunct, dataConjunct), Seq("1", "7", "13", "19"))
+  }
+
+  test("test column stats filter on 2 non-partition column") {
+    // Filter: (i % 3 == 1 AND i % 4 == 1) (1 <= i <= 20)
+    // Output: i = 1 or 13
+    testExpression(new And(new EqualTo(schema.column("col2"), Literal.of(1L)),
+      new EqualTo(schema.column("col3"), Literal.of(1L))), Seq("1", "13"))
+  }
+
+  test("test column stats filter on 1 partition column - impossible") {
+    // Filter: (i % 3 == 1 AND i % 4 == 1) (1 <= i <= 20)
+    // Output: i = 1 or 13
+    testExpression(new And(new EqualTo(schema.column("col2"), Literal.of(1L)),
+      new EqualTo(schema.column("col2"), Literal.of(2L))), Seq())
   }
 }
