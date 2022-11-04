d9e5f3b961a67243cd4f8eefae5f51ee3eeb7cfa
Allison Portis <allison.portis@databricks.com>
2022-11-03 19:10:24 -0700

    revert deltaerror changes

--- standalone/src/main/scala/io/delta/standalone/internal/exception/DeltaErrors.scala
+++ standalone/src/main/scala/io/delta/standalone/internal/exception/DeltaErrors.scala
@@ -400,7 +400,6 @@ private[internal] object DeltaErrors {
     new IllegalArgumentException(s"Cannot drop nonexistent constraint '$name'.")
   }
 
-
   def changeColumnMappingModeNotSupported(oldMode: String, newMode: String): Throwable = {
     new ColumnMappingUnsupportedException(
       s"Changing column mapping mode from $oldMode to $newMode is not supported.")
@@ -419,9 +418,9 @@ private[internal] object DeltaErrors {
       oldSchema: StructType, newSchema: StructType): Throwable = {
     new ColumnMappingUnsupportedException(
       "Schema change is detected: \n" +
-        s"old schema: ${oldSchema.getTreeString}\n" +
-        s"new schema: ${newSchema.getTreeString}\n" +
-        "Schema changes are not allowed during the change of column mapping mode.")
+      s"old schema: ${oldSchema.getTreeString}\n" +
+      s"new schema: ${newSchema.getTreeString}\n" +
+      "Schema changes are not allowed during the change of column mapping mode.")
   }
 
   def misformattedInvariant(expr: String): Throwable = {
